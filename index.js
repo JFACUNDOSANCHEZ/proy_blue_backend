5a7d4170d6d1481de0d21305a0863318b1932669
Facu Sanchez <facusanchz96@gmail.com>
2024-01-16 13:40:11 -0300

    Postgres añadido

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 import express from "express";
+import pg from "pg";
+import { config } from "dotenv";
 
-const app = express()
+config();
+
+const app = express();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+// app.get('/hola', async (req, res) => {
+//     const result = await pool.query('SELECT NOW()');
+//     return res.json(result.rows[0]);
+// });
 
 
-app.get('/'), (req, res) => {
-res.express('Hola mundo')
+app.get('/', (req, res) => {
+    res.send('Hola mundo'); 
+});
 
-}
+app.get('/pong', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT NOW()');
+      return res.json(result.rows[0]);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
-app.listen(3000)
-console.log('Server port', 3000);
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
