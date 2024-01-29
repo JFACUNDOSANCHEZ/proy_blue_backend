824ec9f1eb26cbc5f4e6347d93eb20e830f77ba2
Facu Sanchez <facusanchz96@gmail.com>
2024-01-29 16:56:00 -0300

    m

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -149,14 +149,6 @@ router.get('/getUser', async (req, res) => {
   }
 });
 
-router.get('/pong', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    return res.json(result.rows[0]);
-  } catch (error) {
-    console.error('Error executing query:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+
 
 module.exports = router;
