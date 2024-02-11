cf960da9046181d5c4b475cf1728a81af923b69d
Facu Sanchez <facusanchz96@gmail.com>
2024-02-10 21:35:27 -0300

    activo: pendiente

--- src/controllers/putUser.js
+++ src/controllers/putUser.js
@@ -4,12 +4,12 @@ const { sequelize, User, } = require('../../config/sequelize');
 
 const putUser = async (req, res) => {
   const { id } = req.params;
-  const { activo, nombreUsuario, nombreCompleto, nivel } = req.body;
+  const { activo,  nombreCompleto, nivel, pendiente } = req.body;
 console.log(id);
 
   try {
     const usuario = await User.findByPk(id);
-
+console.log('¡aca esta el userrrrr'+ usuario);
     if (!usuario) {
       return res.status(404).json({ mensaje: 'Usuario no encontrado' });
     }
@@ -23,6 +23,14 @@ console.log(id);
 
     if (nivel) {
       usuario.nivel = nivel;
+    }
+    if (pendiente !== undefined) {
+      console.log('El campo pendiente está presente en la solicitud');
+   
+    }
+    if (pendiente) {
+      console.log('aca esta en pendiente' +  pendiente);
+      usuario.pendiente = pendiente;
     }
 
     await usuario.save();
